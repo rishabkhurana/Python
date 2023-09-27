a43a69d5cea9c6637813e5eb76d881394b0b240b
rishabkhurana <rishabkhurana823@gmail.com>
2023-09-27 21:13:29 +0530

    added code

---
+++ set.py
@@ -0,0 +1,11 @@
+"""
+Set
+-> unordered collection of data
+-> duplicates not allowed
+-> indexing & slicing are not allowed
+-> represented in curly brackets
+"""
+
+a = {10,20,30,40,20}
+b = {10,20,30,"security","raj",40}
+print(a)
